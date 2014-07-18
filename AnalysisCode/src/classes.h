512bc9a31a498cbe4cd7b821504f2947dd384999
John <jgw2kb@virginia.edu>
2014-07-18 13:41:03 -0500

    updated to add BEANeventVars_NLO

--- AnalysisCode/src/classes.h
+++ AnalysisCode/src/classes.h
@@ -1,6 +1,7 @@
 //Add includes for your classes here
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "ttH-LeptonPlusJets/AnalysisCode/interface/BEANeventVars.h"
+#include "ttH-LeptonPlusJets/AnalysisCode/interface/BEANeventVars_NLO.h"
 #include <vector>
 
 namespace {
@@ -11,6 +12,11 @@ namespace {
      std::vector<eventVars> eventVarsDummy1;  
      edm::Wrapper<eventVars> eventVarsDumm2;
      edm::Wrapper<std::vector<eventVars> > eventVarsDummy4;
+
+     BEANeventVars_NLO BEANeventVars_NLODummy0;
+     std::vector<BEANeventVars_NLO> BEANeventVars_NLODummy1;  
+     edm::Wrapper<BEANeventVars_NLO> BEANeventVars_NLODumm2;
+     edm::Wrapper<std::vector<BEANeventVars_NLO> > BEANeventVars_NLODummy4;
 
    };
 }
