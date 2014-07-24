a012b8fff5e64a18539044e2f4bf501dfb46585d
John <jgw2kb@virginia.edu>
2014-07-24 14:13:14 -0500

    update stores all stages of tight genLepton

--- AnalysisCode/interface/yggdrasil_NLO.h
+++ AnalysisCode/interface/yggdrasil_NLO.h
@@ -42,13 +42,13 @@
 //*****************************************************************************
 
 typedef std::vector< std::vector< std::vector<double> > > vvvdouble;
-typedef std::vector<std::vector<double> >      vvdouble;
-typedef std::vector<std::vector<std::string> > vvstring;
-typedef std::vector<double>                    vdouble;
-typedef std::vector<string>                    vstring;
-typedef std::vector<bool>                      vbool;
-typedef std::vector<int>                       vint;
-typedef std::vector< TLorentzVector >          vTLV;
+typedef std::vector<std::vector<double> >                 vvdouble;
+typedef std::vector<std::vector<std::string> >            vvstring;
+typedef std::vector<double>                               vdouble;
+typedef std::vector<string>                               vstring;
+typedef std::vector<bool>                                 vbool;
+typedef std::vector<int>                                  vint;
+typedef std::vector< TLorentzVector >                     vTLV;
 
 
 //*****************************************************************************
@@ -87,7 +87,7 @@ class yggdrasil_NLO{
 
   void getGoodGenJets(std::vector< reco::GenJet > genJets,  std::vector< reco::GenJet > &target, double cut_pt, double cut_eta);
 
-  void getGoodGenLeptons(std::vector< reco::GenParticle > genParticles,  std::vector< reco::GenParticle > &target, bool getTightLeptons, bool getLooseLeptons, double cut_tight_pt, double cut_tight_eta, double cut_loose_pt, double cut_loose_eta);
+  void getGoodGenLeptons(std::vector< reco::GenParticle > genParticles,  std::vector< std::vector< reco::GenParticle > > &target, bool getTightLeptons, bool getLooseLeptons, double cut_tight_pt, double cut_tight_eta, double cut_loose_pt, double cut_loose_eta);
 
   void getGenJetsMatchedToTTbarSystem(std::vector< reco::GenParticle > ttbar_system_hadrons, std::vector< reco::GenJet > genJets, double deltaR_match, bool topIsLeptonic, reco::GenParticle b_quark_fromTop_hadrons, reco::GenParticle wDaughter1_fromTop_hadrons, reco::GenParticle wDaughter2_fromTop_hadrons, bool topBarIsLeptonic, reco::GenParticle b_quark_fromTopBar_hadrons, reco::GenParticle wDaughter1_fromTopBar_hadrons, reco::GenParticle wDaughter2_fromTopBar_hadrons, reco::GenJet &genJet_b_quark_fromTop, reco::GenJet &genJet_wDaughter1_fromTop, reco::GenJet &genJet_wDaughter2_fromTop, reco::GenJet &genJet_b_quark_fromTopBar, reco::GenJet &genJet_wDaughter1_fromTopBar, reco::GenJet &genJet_wDaughter2_fromTopBar);
 
