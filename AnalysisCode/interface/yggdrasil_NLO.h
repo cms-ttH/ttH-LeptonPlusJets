4ffd05164960136ef2a686148af405affae8f113
John <jgw2kb@virginia.edu>
2014-07-24 10:33:00 -0500

    updated BEANeventVars_NLO with more variables

--- AnalysisCode/interface/yggdrasil_NLO.h
+++ AnalysisCode/interface/yggdrasil_NLO.h
@@ -41,14 +41,14 @@
 
 //*****************************************************************************
 
-
+typedef std::vector< std::vector< std::vector<double> > > vvvdouble;
 typedef std::vector<std::vector<double> >      vvdouble;
 typedef std::vector<std::vector<std::string> > vvstring;
 typedef std::vector<double>                    vdouble;
 typedef std::vector<string>                    vstring;
 typedef std::vector<bool>                      vbool;
 typedef std::vector<int>                       vint;
-typedef std::vector< TLorentzVector >          vecTLorentzVector;
+typedef std::vector< TLorentzVector >          vTLV;
 
 
 //*****************************************************************************
@@ -77,11 +77,22 @@ class yggdrasil_NLO{
 
   void getDecayChainFinalState(std::vector< reco::GenParticle >::const_iterator parent, std::vector< reco::GenParticle > genParticles, std::vector< reco::GenParticle > &target);
 
+  void getGenTTbarSystem(std::vector< reco::GenParticle > genParticles, std::vector< reco::GenParticle > &top_quark, std::vector< reco::GenParticle > &b_quark_fromTop, std::vector< reco::GenParticle > &w_boson_fromTop, std::vector< reco::GenParticle > &wDaughter1_fromTop, std::vector< reco::GenParticle > &wDaughter2_fromTop, std::vector< reco::GenParticle > &topBar_quark, std::vector< reco::GenParticle > &b_quark_fromTopBar, std::vector< reco::GenParticle > &w_boson_fromTopBar, std::vector< reco::GenParticle > &wDaughter1_fromTopBar, std::vector< reco::GenParticle > &wDaughter2_fromTopBar); 
+
+  void getGenExtraBCQuarks(std::vector< reco::GenParticle > genParticles, std::vector< reco::GenParticle > &vetoCandidates, std::vector< std::vector< reco::GenParticle > > &extraBQuarks, std::vector< std::vector< reco::GenParticle > > &extraCQuarks);
+
+  void getGenQuarksIntitiatingHadronization( std::vector< reco::GenParticle > genParticles, std::vector< reco::GenParticle > &target );
+
   bool isGoodGenJet(reco::GenJet genJet, double cut_pt, double cut_eta);
 
   void getGoodGenJets(std::vector< reco::GenJet > genJets,  std::vector< reco::GenJet > &target, double cut_pt, double cut_eta);
 
   void getGoodGenLeptons(std::vector< reco::GenParticle > genParticles,  std::vector< reco::GenParticle > &target, bool getTightLeptons, bool getLooseLeptons, double cut_tight_pt, double cut_tight_eta, double cut_loose_pt, double cut_loose_eta);
+
+  void getGenJetsMatchedToTTbarSystem(std::vector< reco::GenParticle > ttbar_system_hadrons, std::vector< reco::GenJet > genJets, double deltaR_match, bool topIsLeptonic, reco::GenParticle b_quark_fromTop_hadrons, reco::GenParticle wDaughter1_fromTop_hadrons, reco::GenParticle wDaughter2_fromTop_hadrons, bool topBarIsLeptonic, reco::GenParticle b_quark_fromTopBar_hadrons, reco::GenParticle wDaughter1_fromTopBar_hadrons, reco::GenParticle wDaughter2_fromTopBar_hadrons, reco::GenJet &genJet_b_quark_fromTop, reco::GenJet &genJet_wDaughter1_fromTop, reco::GenJet &genJet_wDaughter2_fromTop, reco::GenJet &genJet_b_quark_fromTopBar, reco::GenJet &genJet_wDaughter1_fromTopBar, reco::GenJet &genJet_wDaughter2_fromTopBar);
+
+  void getGenJetsMatchedToExtraQuarks(std::vector< reco::GenParticle > extraQuarks, std::vector< reco::GenJet > genJets, std::vector< reco::GenJet > genJet_matchedToTTbarSystem, double deltaR_match, std::vector< reco::GenJet > &genJet_matchedToExtraBQuarks, std::vector< reco::GenJet > &genJet_matchedToExtraCQuarks);
+
 
 }; // End of Class Prototype
 
