c3991ff7ac367284ba99ad6dced07481b08ac668
Satoshi Hasegawa <satoshi@fnal.gov>
2016-04-29 02:45:10 -0500

    Grid Job Time tuned.

--- YggdrasilTreeMaker/test/crabconfig_template.py
+++ YggdrasilTreeMaker/test/crabconfig_template.py
@@ -24,7 +24,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '__yggdrasil_treeMaker_QQQQQ_cfg.py'
 
 config.JobType.allowUndistributedCMSSW = True
-config.JobType.maxJobRuntimeMin = 10000 # default 1315 = 21 hours.
+config.JobType.maxJobRuntimeMin = 600 # default 1315 = 21 hours.
 config.JobType.inputFiles = ['Fall15_25nsV2_DATA.db']
 
 ##########################################
@@ -43,3 +43,4 @@ config.Data.publication = True
 
 
 config.Site.storageSite = 'T3_US_FNALLPC'
+
